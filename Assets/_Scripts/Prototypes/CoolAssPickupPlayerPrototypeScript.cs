f61c85b411a90c270df962c886bc5a3e81cbe259
ValdemarHermannss <valdi04h@gmail.com>
2025-01-24 15:23:57 +0100

    Item pick up thing

--- Assets/_Scripts/Prototypes/CoolAssPickupPlayerPrototypeScript.cs
+++ Assets/_Scripts/Prototypes/CoolAssPickupPlayerPrototypeScript.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using TMPro;
 
 public class CoolAssPickupPlayerPrototypeScript : MonoBehaviour
 {
     public bool canPickup = false;
-    public int numberOfFlowers;
-    public GameObject targetItem;
+    public int numberOfFlowers = 0;
+    public GameObject targetItem = null;
+    public TMP_Text questText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,5 +28,7 @@ public class CoolAssPickupPlayerPrototypeScript : MonoBehaviour
     
         Destroy(targetItem);
         numberOfFlowers += 1;
+        canPickup = false;
+        targetItem = null;
     }
 }
