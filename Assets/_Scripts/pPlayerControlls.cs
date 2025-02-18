d0719fcc59245d774ab2ff39ea33bffa67627e4e
Ben <bebru0048@student.nord.no>
2025-02-18 15:21:39 +0100

    ready to shoot ui code

--- Assets/_Scripts/pPlayerControlls.cs
+++ Assets/_Scripts/pPlayerControlls.cs
@@ -11,6 +11,8 @@ public class pPlayerControlls : MonoBehaviour
     [SerializeField] float jumpHeight = 4f;
     float gravity = -9.81f;
 
+    [SerializeField] GameObject canShootIndicator;
+
     private pPlayerComponent playerData;
 
     [SerializeField] float pebbleSpeed = 1800f;
@@ -90,6 +92,11 @@ public class pPlayerControlls : MonoBehaviour
                     if (chargeTime >= .6)
                     {
                         Debug.Log("ReadyToFire");
+
+                        // can fire ui
+                        canShootIndicator.SetActive(true);
+
+                        // pebble in slingshot scam
                         pebblePlaceHolder.SetActive(true);
                         canFire = true;
                         return;
@@ -188,6 +195,7 @@ public class pPlayerControlls : MonoBehaviour
         newPebble.tag = "Pebble";
 
         playerData.DecreasePebbleCount(1);
+        canShootIndicator.SetActive(false);
     }
 
     void ResetSlingshot()
