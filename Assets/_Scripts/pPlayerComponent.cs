70590bc44cc8f6770a2c812cf31e99d752f6984a
Ben <bebru0048@student.nord.no>
2025-02-18 16:47:23 +0100

    fixu now unity?????!

--- Assets/_Scripts/pPlayerComponent.cs
+++ Assets/_Scripts/pPlayerComponent.cs
@@ -167,6 +167,8 @@ public class pPlayerComponent : MonoBehaviour
 
     void InitializePebblesHUD(int count)
     {
+        Debug.Log("initiated pebble ui with max pebbles: " + count + " / " + maxPebbles);
+        Debug.Log(" before: pebble field = " + pebbleHUDField.gameObject.name + " pebbleui = " + pebbleUIPrefab.gameObject.name);
         foreach (Transform child in pebbleHUDField.transform)
         {
             Destroy(child.gameObject);
@@ -179,6 +181,8 @@ public class pPlayerComponent : MonoBehaviour
             RectTransform rectTransform = pebbleUIElement.GetComponent<RectTransform>();
             rectTransform.anchoredPosition = new Vector2(i * 100, 0);
         }
+        Debug.Log("finished initializing pebble ui with max pebbles: " + count + " / " + maxPebbles + " amount initiated = " + pebbleHUDField.transform.childCount);
+        Debug.Log(" after: pebble field = " + pebbleHUDField.gameObject.name + " pebbleui = " + pebbleUIPrefab.gameObject.name);
     }
 
     void DecreasePebbleHUD()
