d35794433ba4c8c7dc534d104a6a85e29b56ad71
Veronika <verboyanova@gmail.com>
2025-02-21 13:23:53 +0100

    Ghosts wandering

---
+++ Assets/_Scripts/Ghosts/Wandering.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wandering : MonoBehaviour
+{
+   public float wanderRadius = 10f; // The range within which the character will wander
+    public float wanderTimer = 5f; // Time before choosing a new destination
+
+    private NavMeshAgent agent;
+    private float timer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * dist;
+        randomDirection += origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomDirection, out navHit, dist, layermask);
+        return navHit.position;
+    }
+}
