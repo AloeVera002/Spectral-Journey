5a95599a5ea0be1e51fea956c53623f61ef4c305
Veronika <verboyanova@gmail.com>
2025-02-28 11:02:57 +0100

    Wandering ghosts

--- Assets/_Scripts/NPC/Wandering.cs
+++ Assets/_Scripts/NPC/Wandering.cs
@@ -1,9 +1,10 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Wandering : MonoBehaviour
 {
-   public float wanderRadius = 10f; // The range within which the character will wander
+  /* public float wanderRadius = 10f; // The range within which the character will wander
     public float wanderTimer = 5f; // Time before choosing a new destination
 
     private NavMeshAgent agent;
@@ -34,6 +35,52 @@ public class Wandering : MonoBehaviour
         NavMeshHit navHit;
         NavMesh.SamplePosition(randomDirection, out navHit, dist, layermask);
         return navHit.position;
+    }*/
+
+     public Transform centerPoint; // Empty GameObject as the wander center
+    public float wanderRadius = 5f; // Maximum wander distance
+    public float waitTime = 2f; // Time before picking a new destination
+    public float minDistance = 0.5f; // Distance considered as "arrived"
+
+    private NavMeshAgent agent;
+    private bool isWaiting = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        ChooseNewTarget();
+    }
+
+    void Update()
+    {
+        if (!isWaiting && agent.remainingDistance <= minDistance)
+        {
+            StartCoroutine(WaitBeforeNextMove());
+        }
+    }
+
+    void ChooseNewTarget()
+    {
+        Vector2 randomPoint = Random.insideUnitCircle * wanderRadius;
+        Vector3 targetPosition = centerPoint.position + new Vector3(randomPoint.x, 0, randomPoint.y);
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(targetPosition, out hit, wanderRadius, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hit.position);
+        }
+        else
+        {
+            ChooseNewTarget(); // Try again if no valid point was found
+        }
+    }
+
+    IEnumerator WaitBeforeNextMove()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTime);
+        isWaiting = false;
+        ChooseNewTarget();
     }
 
     //this code is made with the help of chatGPT - Veronika
